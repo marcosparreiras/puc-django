3d0f13a166c3710ab30fbfac585b9c9468b1663a
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-25 12:35:11 -0300

    feat: change user model to student model on school-api

--- school_app_api/models.py
+++ school_app_api/models.py
@@ -1,23 +1,14 @@
 from django.db import models
-import datetime
-import uuid 
 
-class User(models.Model):
-    id = models.UUIDField( 
-         primary_key = True, 
-         default = uuid.uuid4, 
-         editable = False)
+
+class Student(models.Model):
+    student_code = models.IntegerField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=80, null=False)
     date_of_birth = models.DateField(null=False)
-    address_street = models.CharField(max_length=80, null=False)
-    address_number = models.IntegerField(null= False)
-    registration_number = models.IntegerField(null=True)
-    is_principal = models.BooleanField(null=True)
-    is_employee = models.CharField(null=True)
-    is_student = models.CharField(null=True)
+    created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(null=True)
-    created_at = models.DateField(default=datetime.date.today)
+    address_street = models.CharField(max_length=80, null=False)
+    address_number = models.IntegerField(null=False)
 
     def __str__(self):
-        return f"({self.id}) {self.name}"
-
+        return f"({self.student_code}) {self.name}"
