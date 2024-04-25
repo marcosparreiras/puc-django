70a95aeac7f55c0d98d99c074bd43478ed943786
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-25 01:52:46 -0300

    feat: create a user model on school_app_api

--- school_project/school_app_api/models.py
+++ school_project/school_app_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
+import uuid 
 
-# Create your models here.
+class User(models.Model):
+    id = models.UUIDField( 
+         primary_key = True, 
+         default = uuid.uuid4, 
+         editable = False)
+    name = models.CharField(max_length=80, null=False)
+    date_of_birth = models.DateField(null=False)
+    address_street = models.CharField(max_length=80, null=False)
+    address_number = models.IntegerField(null= False)
+    registration_number = models.IntegerField(null=True)
+    is_principal = models.BooleanField(null=True)
+    is_employee = models.CharField(null=True)
+    is_student = models.CharField(null=True)
+    updated_at = models.DateField(null=True)
+    created_at = models.DateField(default=datetime.date.today)
+
