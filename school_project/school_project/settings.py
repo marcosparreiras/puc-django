438f5bfd3702caf872e633e77b60bb922c43866d
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-25 01:02:45 -0300

    chore: config school_project access to postgres database

--- school_project/school_project/settings.py
+++ school_project/school_project/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'school_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'school_db', 
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
